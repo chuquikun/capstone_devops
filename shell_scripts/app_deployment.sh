ab6525c11aea538c8f023379cd516779eed92a08
Fer Nando <fernando@hendrix.headup.local>
2023-05-01 22:52:46 -0600

    kubectl running

--- shell_scripts/app_deployment.sh
+++ shell_scripts/app_deployment.sh
@@ -5,18 +5,19 @@ LOAD_BALANCER=loadbalancer-service
 # kubernetes cluster is needed
 ${KUBECTL} get nodes
 # deploy app to the to kubernetes cluster
-${KUBECTL} apply -f manifests/deployment_manifest.yml
-# use a load balancer service to access externally the application
-
-if ${KUBECTL} get service  | grep -q ${LOAD_BALANCER}; then
-    echo "Load balancer exists!"
-else
-    echo "Creating  load balancer service..."
-    ${KUBECTL} create -f manifests/lb_manifest.yml
-fi
-
-# the external-ip for the service can be retrieved from this output
-${KUBECTL} get service/loadbalancer-service |  awk {'print $1" " $2 " " $4 " " $5'} | column -t
-# See the status
-${KUBECTL} get deploy,rs,svc,pods
-
+#${KUBECTL} apply -f manifests/deployment_manifest.yml
+## use a load balancer service to access externally the application
+#
+#if ${KUBECTL} get service  | grep -q ${LOAD_BALANCER}; then
+#    echo "Load balancer exists!"
+#else
+#    echo "Creating  load balancer service..."
+#    ${KUBECTL} create -f manifests/lb_manifest.yml
+#fi
+#
+## the external-ip for the service can be retrieved from this output
+#${KUBECTL} get service/loadbalancer-service
+## See the status
+#${KUBECTL} get deploy,rs,svc,pods
+#
+#
