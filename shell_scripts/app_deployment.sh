df254c9cd2e7346dcda9217aefe39b2fecc84741
Fer Nando <fernando@hendrix.headup.local>
2023-05-02 08:51:30 -0600

    kubeconfig

--- shell_scripts/app_deployment.sh
+++ shell_scripts/app_deployment.sh
@@ -6,18 +6,17 @@ CONTAINER_PORT=80
 #${KUBECTL} get nodes
 # deploy app to the to kubernetes cluster
 ${KUBECTL} apply -f manifests/deployment_manifest.yml
-## use a load balancer service to access externally the application
-#
+# use a load balancer service to access externally the application
+
 #if ${KUBECTL} get service  | grep -q ${LOAD_BALANCER}; then
 #    echo "Load balancer exists!"
 #else
 #    echo "Creating  load balancer service..."
 #    ${KUBECTL} create -f manifests/lb_manifest.yml
 #fi
-#
-## the external-ip for the service can be retrieved from this output
-#${KUBECTL} get service/loadbalancer-service
-## See the status
-#${KUBECTL} get deploy,rs,svc,pods
-#
-#
+
+# the external-ip for the service can be retrieved from this output
+${KUBECTL} get service/loadbalancer-service
+# See the status
+${KUBECTL} get deploy,rs,svc,pods
+
