ac2edb0f3d7a7f2891b0ae01de71bce45700cd32
Fer Nando <fernando@hendrix.headup.local>
2023-05-01 19:50:25 -0600

    create infra

--- shell_scripts/create_cluster.sh
+++ shell_scripts/create_cluster.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 
-if /bin/kubectl config get-clusters | grep -q dashboard; then
+if kubectl config get-clusters | grep -q dashboard; then
     echo "Cluster already exists!"
 else
     echo "Creating..."
-    /bin/eksctl create cluster -f manifests/cluster.yml
+    eksctl create cluster -f manifests/cluster.yml
 fi
