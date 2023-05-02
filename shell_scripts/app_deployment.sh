189ca6ee8861bcc07697a27a60e7f1cbd5169fe6
Fer Nando <fernando@hendrix.headup.local>
2023-05-01 21:26:25 -0600

    fix kubectl installation

--- shell_scripts/app_deployment.sh
+++ shell_scripts/app_deployment.sh
@@ -2,20 +2,20 @@
 LOAD_BALANCER=loadbalancer-service
 
 # kubernetes cluster is needed
-/bin/kubectl get nodes
+./bin/kubectl get nodes
 # deploy app to the to kubernetes cluster
-/bin/kubectl apply -f manifests/deployment_manifest.yml
+./bin/kubectl apply -f manifests/deployment_manifest.yml
 # use a load balancer service to access externally the application
 
-if /bin/kubectl get service  | grep -q ${LOAD_BALANCER}; then
+if ./bin/kubectl get service  | grep -q ${LOAD_BALANCER}; then
     echo "Load balancer exists!"
 else
     echo "Creating  load balancer service..."
-    /bin/kubectl create -f manifests/lb_manifest.yml
+    ./bin/kubectl create -f manifests/lb_manifest.yml
 fi
 
 # the external-ip for the service can be retrieved from this output
-/bin/kubectl get service/loadbalancer-service |  awk {'print $1" " $2 " " $4 " " $5'} | column -t
+./bin/kubectl get service/loadbalancer-service |  awk {'print $1" " $2 " " $4 " " $5'} | column -t
 # See the status
-/bin/kubectl get deploy,rs,svc,pods
+./bin/kubectl get deploy,rs,svc,pods
 
